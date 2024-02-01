38bccb019632c4f8c35d313e3a03795f2ce353e0
jack-kower <kowerjack@gmail.com>
2024-01-31 16:18:34 -0800

    fixed user view to pass test

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,7 @@
 """
 Views for User API.
 """
-from rest_framework import generics, authenication, permissions
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer, AuthTokenSerializer
@@ -21,7 +21,7 @@ class CreateTokenView(ObtainAuthToken):
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """manage the authenticated user"""
     serializer_class = UserSerializer
-    authentication_classes = [authenication.TokenAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
